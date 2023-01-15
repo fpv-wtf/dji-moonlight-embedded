9049e55098c6cb383a29ca82a5f7ddcee2cdfcec
Daniel Callander <knifacat@gmail.com>
2023-01-15 13:03:40 +0000

    send header

--- src/video/dji.c
+++ src/video/dji.c
@@ -10,26 +10,38 @@
 #include <sys/socket.h>
 
 int sockfd = -1;
+int frame_counter = 0;
 
-static int dji_setup(int videoFormat, int width, int height, int redrawRate, void *context, int drFlags)
+typedef struct connect_header_s
 {
-    if (videoFormat != VIDEO_FORMAT_H264)
-    {
-        printf("dji_setup: Unsupported video format %d\n", videoFormat);
-        return 1;
-    }
+    uint32_t magic;
+    uint32_t width;
+    uint32_t height;
+    uint32_t fps;
+} connect_header_t;
 
-    printf("dji_setup: %d x %d @ %d FPS\n", width, height, redrawRate);
-    printf("dji_setup: context = %p\n", context);
-    printf("dji_setup: drFlags = 0x%x\n", drFlags);
+static int
+dji_setup(int videoFormat, int width, int height, int redrawRate, void *context, int drFlags)
+{
+    // if (videoFormat != VIDEO_FORMAT_H264)
+    // {
+    //     printf("dji_setup: Unsupported video format %d\n", videoFormat);
+    //     return 1;
+    // }
+
+    printf("dji: %d x %d @ %d FPS\n", width, height, redrawRate);
+    printf("dji: context = %p\n", context);
+    printf("dji: drFlags = 0x%x\n", drFlags);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(5123);
+    server_addr.sin_port = htons(42069);
     server_addr.sin_addr.s_addr = inet_addr("192.168.42.5");
     memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
+
+    printf("dji: Connecting to %s:%d...\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 
     int connect_ret = 0;
     while ((connect_ret = connect(sockfd, &server_addr, sizeof(server_addr))) < 0)
@@ -38,17 +50,22 @@ static int dji_setup(int videoFormat, int width, int height, int redrawRate, voi
         sleep(1);
     }
 
-    return 0;
-}
+    printf("dji: Connected!\n");
 
-static void dji_cleanup()
-{
-    printf("dji_cleanup\n");
+    connect_header_t header;
+    header.magic = 0x42069;
+    header.width = width;
+    header.height = height;
+    header.fps = redrawRate;
+
+    send(sockfd, &header, sizeof(header), 0);
+
+    return 0;
 }
 
 static int dji_submit_decode_unit(PDECODE_UNIT decodeUnit)
 {
-    printf("dji_submit_decode_unit: frameNumber: %d, frameType: %d, fullLength: %d\n",
+    printf("dji frameNumber: %d, frameType: %d, fullLength: %d\n",
            decodeUnit->frameNumber, decodeUnit->frameType, decodeUnit->fullLength);
 
     uint8_t *buf = malloc(decodeUnit->fullLength);
@@ -74,6 +91,5 @@ static int dji_submit_decode_unit(PDECODE_UNIT decodeUnit)
 
 DECODER_RENDERER_CALLBACKS decoder_callbacks_dji = {
     .setup = dji_setup,
-    .cleanup = dji_cleanup,
     .submitDecodeUnit = dji_submit_decode_unit,
     .capabilities = 0};
